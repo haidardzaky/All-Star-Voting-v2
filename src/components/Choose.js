875835b46a8b3208dc20cbcb7b710d0ef6c662f3
haidardzaky <haidardz@gmail.com>
2018-02-08 14:22:40 +0700

    little fix

--- src/components/Choose.js
+++ src/components/Choose.js
@@ -1,16 +1,2 @@
-import React from "react"
-import { Button } from 'semantic-ui-react'
-import './Component.css'
-
-const Choose = () => (
-  <div className="choose">
-    <h3 className="H3-title">Choose your favorite players !</h3>
-    <Button.Group>
-      <Button color='blue' className="backcourt-button">Backcourt</Button>
-      <Button color='red' className="frontcourt-button">Frontcourt</Button>
-    </Button.Group>
-
-  </div>
-)
-
-export default Choose
+import react from 'React'
+import
